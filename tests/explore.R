c956c71aab7dea89ba07ddf114cd1a77e1cf7cc3
Stefan Bentink <bentink@dkms-lab.de>
2017-05-08 09:49:40 +0200

    set read quality threshold to zero to work with Sequel data

--- tests/explore.R
+++ tests/explore.R
@@ -10,6 +10,12 @@ x <- DR2Smap(
   threshold = 0.2
 )
 
+
+x <- DR2S::read_dr2s("~/dr2s_data/output/BOLETH/pacbio/KIR3DL2.pacbio.ref.multialign/")
+x
+
+##run_igv(x, 100)
+
 y1 <- x %>%
   map0() %>%
   partition_haplotypes() %>%
@@ -23,4 +29,31 @@ y3 <- y2 %>%
   map3() %>%
   polish() %>%
   report()
+
+x2 <- DR2Smap(
+  sample = "HO104",
+  locus = "KIR2DL4",
+  longreads = list(type = "pacbio", dir = "pacbio"),
+  shortreads = list(type = "illumina", dir = "illumina"),
+  datadir = "~/dr2s_data",
+  outdir = "~/dr2s_data/output",
+  reference = "KIR2DL4*00501",
+  consensus = "multialign",
+  threshold = 0.2
+)
+
+x2_r <- x2 %>%
+  map0(min_base_quality = 0)# %>%
+  #partition_haplotypes() %>%
+  #split_reads_by_haplotype() %>%
+  #extract_fastq()
+
+#y2 <- y1 %>% map1()
+
+#y3 <- y2 %>%
+#  map2() %>%
+#  map3() %>%
+#  polish() %>%
+#  report()
+
 
