103620e735622b7442c2654005a38d741d4651e1
Gerhard Schöfl <Schoefl@dkms-lab.de>
2018-12-14 20:18:34 +0100

    on devel branch rename pkg to DR2Sdevel

--- R/DR2S-package.R
+++ R/DR2S-package.R
@@ -5,8 +5,8 @@
 #' sequencing technologies
 #'
 #' @docType package
-#' @name DR2S
-#' @useDynLib DR2S
+#' @name DR2Sdevel
+#' @useDynLib DR2Sdevel
 #' @import assertthat
 #' @import futile.logger
 #' @import ggplot2
